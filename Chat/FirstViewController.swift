847a8f46aa4a6cef7c07145e56fc22ba491edc2f
yt19860606 <yt510456345@aliyun.com>
2019-04-25 09:48:04 +0800

    Initial Commit

---
+++ Chat/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Chat
+//
+//  Created by yetao on 2019/4/25.
+//  Copyright © 2019 yetao. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Chat/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Chat
+//
+//  Created by yetao on 2019/4/25.
+//  Copyright © 2019 yetao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
